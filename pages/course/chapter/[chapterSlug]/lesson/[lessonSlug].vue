0ea13647a067d1a2cad649edf6591f11f863105c
Mohsin Raazi <mohsinrazakhan4455@gmail.com>
2024-08-10 23:42:50 +0000

    1-6: render the course content to the browser

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -20,12 +20,31 @@ const lesson = computed(() => {
     (lesson) => lesson.slug === route.params.lessonSlug
   );
 });
-// console.log(lesson.value)
+console.log(lesson.value)
 </script>
 
 <template>
-  <h2>Lesson</h2>
-  <p>This is a lesson</p>
-  <p>chapter-slug:{{ chapter?.title }}</p>
-  <!-- <p>lesson-slug:{{ lesson?.title }}</p> -->
+  <div>
+    <p class="mt-0 uppercase font-bold text-slate-400 mb-1">
+      Lesson {{ chapter?.number }} - {{ lesson?.number }}
+    </p>
+    <h2 class="my-0">{{ lesson?.title }}</h2>
+    <div class="flex space-x-4 mt-2 mb-8">
+      <a
+        v-if="lesson?.sourceUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.sourceUrl"
+      >
+        Download Source Code
+      </a>
+      <a
+        v-if="lesson?.downloadUrl"
+        class="font-normal text-md text-gray-500"
+        :href="lesson.downloadUrl"
+      >
+        Download Video
+      </a>
+    </div>
+    <p>{{ lesson?.text }}</p>
+  </div>
 </template>
