ac44a99bd1fcd5d970183660c2580cc3a7c75e47
Mohsin Raazi <mohsinrazakhan4455@gmail.com>
2024-08-11 01:40:48 +0000

    1-9: navigating with nuxt link

--- pages/course.vue
+++ pages/course.vue
@@ -13,17 +13,31 @@
 
     <div class="flex flex-row justify-center flex-grow">
       <div
-        class="prose mr-4 p-8 bg-white rounded-md min-w-[20ch] flex flex-col"
+        class="prose mr-4 p-8 bg-white rounded-md w-[40ch] flex flex-col"
       >
         <h3>Chapters</h3>
-        <!-- All the lessons for the course listed here -->
+        <div class="flex flex-col" v-for="chapter in course.chapters" :key="chapter.slug">
+          <h3>{{ chapter.title }}</h3>
+          <NuxtLink
+            class="no-underline"
+            v-for="(lesson, index) in chapter.lessons"
+            :key="lesson.slug"
+            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+          >
+            <span class="text-gray-500">{{ index + 1 }}. </span>
+            <span>{{ lesson.title }}</span>
+          </NuxtLink>
+        </div>
       </div>
 
-      <div class="prose p-12  bg-pink-200 rounded-md w-[1400px]">
-        <NuxtPage/>
-      </div>  
+      <div class="prose p-12 bg-pink-200 rounded-md w-[70ch]">
+        <NuxtPage />
+      </div>
     </div>
   </div>
 </template>
 
-<script></script>
+<script setup lang="ts">
+import { useCourse } from "~/composable/Course";
+const course = useCourse();
+</script>
